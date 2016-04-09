8a1947dbca1ed536bace35454e7040f508425ab0
heaven-cac <g1444288@gmail.com>
2016-04-09 23:01:09 +0900

    [test]曲の追加
    テスト用の曲を追加しました。

---
+++ MusicKinectTest03/Assets/Sound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+
+    public AudioClip sound01;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A) == true)
+        {
+            // Torigger
+            Debug.Log("A!");
+            GetComponent<AudioSource>().PlayOneShot(sound01);
+        }
+    }
+}
