a1b53a3e297c68511b1989c06ba8eef048fbc8ce
Yuka Kanegae <g1444431@gmail.com>
2016-10-30 11:21:32 +0900

    ジャンプノーツとぐるぐるノーツの配置修正
    ジャンプノーツとぐるぐるノーツの配置を間違えていたため、修正

--- MusicKinectGame/Assets/Play_Game/Scripts/Lane.cs
+++ MusicKinectGame/Assets/Play_Game/Scripts/Lane.cs
@@ -9,7 +9,7 @@ public class NoteData{
 }
 
 public class Lane : MonoBehaviour {
-	public    Vector3 startPoint    =  new Vector3(-2.0f, -28.0f, 88.0f);
+	public    Vector3 startPoint    =  new Vector3(-5.0f, -28.0f, 60.0f);
     protected int     nextTimingNum = 0;
 	protected int     nextObjectNum = 0;
 	public    float   offScreenPos  = -5.0f;
