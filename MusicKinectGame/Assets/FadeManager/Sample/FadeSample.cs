6095d75875bce7b4f4eb47239ff38029a119cd22
syou4 <g1444693@gmail.com>
2016-11-03 03:01:34 +0900

    シーン遷移フェードイン効果のスクリプトインポート
    参考URL：https://github.com/naichilab/Unity-FadeManager/blob/master/README.ja.md

---
+++ MusicKinectGame/Assets/FadeManager/Sample/FadeSample.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeSample : MonoBehaviour
+{
+
+	public void FadeScene ()
+	{
+		FadeManager.Instance.LoadLevel ("Make_Object", 2.0f);
+	}
+}
