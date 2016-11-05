c897f76727db64fb2ed832a59c946d53e1dd7780
syou4 <g1444693@gmail.com>
2016-11-06 00:59:08 +0900

    スコアデータの外部ファイル出力を実装
    SaveData.csはテスト用に作ったスクリプトで、実際は使っていない。
    
    ResultSystem.csに、スコアデータを「Assets/score.txt」に保存する処理を追加した。毎回リザルト画面に遷移する度にこのファイルを参照し、その時のスコアを含めて改めて出力する。
    
    また、ランキングの表示では、自分のスコアの右に「矢印」が出てくるようにした。これに伴い、ランキングは上位5つのスコアだけでなく18位まで表示される。

---
+++ MusicKinectGame/Assets/Play_Game/Scripts/SaveData.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class SaveData : AllSystem{
+
+	public string saveDataPath = "Assets/score.txt";
+	private FileStream f;
+
+	void Start ()
+	{
+		if (File.Exists(saveDataPath))
+		{
+			RoadText(saveDataPath);
+		}
+		scores.Add(10);
+		scores.Add(200);
+		scores.Sort();
+		WriteText(saveDataPath);
+	}
+
+	public void WriteText(string _filePath)
+	{
+		//f = new FileStream(_filePath, FileMode.Create, FileAccess.Write);
+		//BinaryWriter writer = new BinaryWriter(f);
+		StreamWriter sw;
+		sw = new StreamWriter(_filePath);
+		for (int i = 0; i < scores.Count; i++)
+		{
+			sw.WriteLine(scores[i]);
+			//writer.Write(scores[i]);
+		}
+		sw.Close();
+		//writer.Close();
+	}
+
+	public void RoadText(string _filePath)
+	{
+		f = new FileStream(_filePath, FileMode.Open, FileAccess.Read);
+		StreamReader reader = new StreamReader(f);
+		if (reader != null)
+		{
+			while (!reader.EndOfStream)
+			{
+				string str = reader.ReadLine();
+				scores.Add(int.Parse(str));
+				//print(str);
+			}
+			reader.Close();
+		}
+	}
+
+
+
+	void Update () {
+	
+	}
+}
